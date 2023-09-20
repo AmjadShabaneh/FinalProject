eb489c2912951f0760ee6f3d954421aaf042930e
Amjad.Sh <213510@ppu.edu.ps>
2023-09-20 19:01:28 +0300

    setting up tailwind css and tw elements

---
+++ Gproject/resources/views/landing.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p class="text-center">final project</p>
+</body>
+</html>
